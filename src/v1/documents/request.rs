ab02db5746e5db44f80ee8cf9ddc83938f50c422
bouzuya <m@bouzuya.net>
2024-04-22 06:43:00 +0900

    Import files from bouzuya/rust-sandbox#5336c96f04660e0994da22573f417b5073cbf429

---
+++ src/v1/documents/request.rs
@@ -0,0 +1,97 @@
+use crate::google_docs_client::v1::documents::Range;
+use crate::google_docs_client::v1::documents::Size;
+
+/// <https://developers.google.com/docs/api/reference/rest/v1/documents/request#request>
+#[derive(Clone, Debug, Default, PartialEq, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Request {
+    #[serde(flatten)]
+    pub request: Option<RequestRequest>,
+}
+
+#[derive(Clone, Debug, PartialEq, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum RequestRequest {
+    ReplaceAllText(ReplaceAllTextRequest),
+    InsertText(InsertTextRequest),
+    // TODO: ...
+    DeleteContentRange(DeleteContentRangeRequest),
+    InsertInlineImage(InsertInlineImageRequest),
+    // TODO: ...
+}
+
+/// <https://developers.google.com/docs/api/reference/rest/v1/documents/request#replacealltextrequest>
+#[derive(Clone, Debug, Default, PartialEq, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ReplaceAllTextRequest {
+    pub replace_text: Option<String>,
+    #[serde(flatten)]
+    pub criteria: Option<ReplaceAllTextRequestCriteria>,
+}
+
+/// <https://developers.google.com/docs/api/reference/rest/v1/documents/request#replacealltextrequest>
+#[derive(Clone, Debug, PartialEq, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum ReplaceAllTextRequestCriteria {
+    ContainsText(SubstringMatchCriteria),
+}
+
+/// <https://developers.google.com/docs/api/reference/rest/v1/documents/request#substringmatchcriteria>
+#[derive(Clone, Debug, Default, PartialEq, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SubstringMatchCriteria {
+    pub text: Option<String>,
+    pub match_case: Option<bool>,
+}
+
+/// <https://developers.google.com/docs/api/reference/rest/v1/documents/request#insertinlineimagerequest>
+#[derive(Clone, Debug, Default, PartialEq, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct InsertInlineImageRequest {
+    pub uri: Option<String>,
+    pub object_size: Option<Size>,
+    #[serde(flatten)]
+    pub insertion_location: Option<InsertInlineImageRequestInsertionLocation>,
+}
+
+/// <https://developers.google.com/docs/api/reference/rest/v1/documents/request#insertinlineimagerequest>
+#[derive(Clone, Debug, PartialEq, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum InsertInlineImageRequestInsertionLocation {
+    Location(Location),
+    EndOfSegmentLocation(EndOfSegmentLocation),
+}
+
+#[derive(Clone, Debug, Default, PartialEq, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DeleteContentRangeRequest {
+    pub range: Option<Range>,
+}
+
+#[derive(Clone, Debug, Default, PartialEq, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct InsertTextRequest {
+    pub text: Option<String>,
+    #[serde(flatten)]
+    pub insertion_location: Option<InsertTextRequestInsertionLocation>,
+}
+
+#[derive(Clone, Debug, PartialEq, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum InsertTextRequestInsertionLocation {
+    Location(Location),
+    EndOfSegmentLocation(EndOfSegmentLocation),
+}
+
+#[derive(Clone, Debug, Default, PartialEq, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Location {
+    pub segment_id: Option<String>,
+    pub index: Option<usize>,
+}
+
+#[derive(Clone, Debug, Default, PartialEq, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct EndOfSegmentLocation {
+    pub segment_id: Option<String>,
+}
